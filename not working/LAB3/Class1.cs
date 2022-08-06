3b1774dc35039db4cde565c0b7fcc2036cdd2d60
Yonni22 <107666549+Yonni22@users.noreply.github.com>
2022-08-06 10:46:09 +0300

    not working

---
+++ not working/LAB3/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3
+{
+    internal class ProductClass
+    {
+        private static List<ProductClass> products = new List<ProductClass>();
+
+        public int number { get; set; }
+        public string Objectname { get; set; }
+        public int inventorynumber { get; set; }
+        public DateTime date { get; set; }
+        public double price { get; set; }
+        public int Count { get; set; }
+        
+        public bool isavailable { get; set; }
+        public void Save()
+        {
+            Console.WriteLine("created");
+            products.Add(this);
+           
+        }
+        public static List<ProductClass> GetAllProducts()
+        {
+            return products;
+        }
+    }
+}
